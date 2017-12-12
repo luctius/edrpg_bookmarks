a1572890088a2a648b114dc3221f1bdd2ad59f1a
Cor <prive@corpeters.nl>
2017-12-12 16:39:41 +0100

    Updated for 1.1

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 FILE=edrpg_bookmarks.txt
-PDF=edrpg_corebook_download_orig.pdf
+PDF=edrpg_corebook_download_1.1.pdf
+OUT_PDF=edrpg_corebook_download_bookmarked_1.1.pdf
 ./scripts/undo_format.sh ${FILE}
 ./scripts/undo_format.sh ${FILE} tmp.txt
-pdftk ${PDF} update_info tmp.txt output edrpg_corebook_download_bookmarked.pdf
+pdftk ${PDF} update_info tmp.txt output ${OUT_PDF}
 rm -f tmp.txt
